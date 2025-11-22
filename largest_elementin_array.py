a5590afc0da814c2d9783e9a2fd45ebfc97c4f9f
Akash Kumar <121351972+akashkr16@users.noreply.github.com>
2025-11-23 00:04:45 +0530

    Create largest_elementin_array.py

---
+++ largest_elementin_array.py
@@ -0,0 +1,11 @@
+# Given an array, we have to find the largest element in the array.
+
+from typing import List
+def sortArr(arr: List[int]) -> int:
+    arr.sort()
+    return arr[-1]
+if __name__ == "__main__":
+    arr1 = [2, 5, 1, 3, 0]
+    arr2 = [8, 10, 5, 7, 9]
+    print("The Largest element in the array is:", sortArr(arr1))
+    print("The Largest element in the array is:", sortArr(arr2))
